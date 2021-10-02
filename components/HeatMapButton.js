8e0804c6d635ce28b11341d7b195dba3d9cef760
maris <mari.seppanen4@metropolia.fi>
2021-10-02 17:39:53 +0300

    heatmap element

--- components/HeatMapButton.js
+++ components/HeatMapButton.js
@@ -1,30 +1,113 @@
 import React from "react";
-import { Text, View, StyleSheet, Pressable } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import AppLoading from "expo-app-loading";
 
+import {
+  Text,
+  StyleSheet,
+  Pressable,
+  Button,
+  Alert,
+  View,
+  ImageBackground,
+  Image,
+} from "react-native";
+import {
+  useFonts,
+  OpenSans_300Light,
+  OpenSans_300Light_Italic,
+  OpenSans_400Regular,
+  OpenSans_400Regular_Italic,
+  OpenSans_600SemiBold,
+  OpenSans_600SemiBold_Italic,
+  OpenSans_700Bold,
+  OpenSans_700Bold_Italic,
+  OpenSans_800ExtraBold,
+  OpenSans_800ExtraBold_Italic,
+} from "@expo-google-fonts/open-sans";
+const image = { uri: "https://reactjs.org/logo-og.png" };
 export default function HeatMapButton(props) {
-  const { onPress, title = "Save" } = props;
-  return (
-    <Pressable style={styles.button}>
-      <Text style={styles.text}>halloo</Text>
-    </Pressable>
-  );
+  let [fontsLoaded] = useFonts({
+    OpenSans_300Light,
+    OpenSans_400Regular,
+    OpenSans_600SemiBold,
+    OpenSans_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Pressable style={styles.button}>
+          <ImageBackground
+            imageStyle={{ borderRadius: 5 }}
+            source={require("../assets/placeholderMap.jpg")}
+            resizeMode="cover"
+            style={styles.image}
+          >
+            <Text style={styles.text}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </Text>
+            <View style={styles.containerButton}>
+              <View style={styles.buttonContent}>
+                <AntDesign
+                  style={styles.buttonArrow}
+                  name="right"
+                  size={20}
+                  color="#dc0368"
+                />
+              </View>
+            </View>
+          </ImageBackground>
+        </Pressable>
+      </View>
+    );
+  }
 }
 const styles = StyleSheet.create({
-  button: {
-    width: "100%",
+  container: {
+    display: "flex",
     alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  button: {
+    width: 400,
+    backgroundColor: "#FFFF",
+    shadowColor: "#888",
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.8,
+    margin: "5%",
+    alignItems: "flex-start",
     justifyContent: "center",
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 4,
-    elevation: 3,
-    backgroundColor: "#fff",
+    borderRadius: 5,
+  },
+
+  image: {
+    width: "100%",
   },
   text: {
-    fontSize: 16,
-    lineHeight: 21,
-
-    letterSpacing: 0.25,
+    width: "50%",
+    padding: "5%",
+    fontFamily: "OpenSans_600SemiBold",
+    fontSize: 14,
     color: "#000",
+  },
+  containerButton: {
+    padding: "5%",
+    display: "flex",
+    width: "100%%",
+    padding: "2%",
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+  },
+  buttonContent: {
+    padding: "2%",
+    backgroundColor: "#FFFF",
+    shadowColor: "#888",
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    borderRadius: 5,
   },
 });
