73278ca7a433a2d0b435e52ed7359b7ed16f1058
maris <mari.seppanen4@metropolia.fi>
2021-10-07 19:34:05 +0300

    styles to report page

--- components/reports/ReportTopics.js
+++ components/reports/ReportTopics.js
@@ -1,35 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { RadioButton } from 'react-native-paper';
 import ModalDialog from '../globalReUseAbles/ModalDialog';
 import { reportTopics } from './reportConstants';
 
-const ReportTopics = ({ open, closeDialog, checked, setChecked }) => {
+const ReportTopics = ({ checked, setChecked }) => {
   // renders the report topics for user to choose from
   return (
-    <ModalDialog
-      open={open}
-      closeDialog={closeDialog}
-      title="Choose Report Topic"
+    <RadioButton.Group
+      onValueChange={(newValue) => {
+        setChecked(newValue);
+        console.log(newValue);
+      }}
+      value={checked}
     >
-      {reportTopics.map((topic) => (
-        <View style={styles.radio} key={topic}>
-          <RadioButton
-            value={topic}
-            status={checked === topic ? 'checked' : 'unchecked'}
-            onPress={() => setChecked(topic)}
-          />
-          <Text style={styles.text}> {topic} </Text>
-        </View>
-      ))}
-    </ModalDialog>
+      <View style={styles.radioGroup}>
+        {reportTopics.map((topic) => (
+          <View style={styles.radio} key={topic}>
+            <Text>{topic}</Text>
+            <RadioButton
+              style={styles.button}
+              value={topic}
+              status={checked === topic ? 'checked' : 'unchecked'}
+              onPress={() => {
+                setChecked(topic);
+              }}
+            />
+          </View>
+        ))}
+      </View>
+    </RadioButton.Group>
   );
 };
 
 const styles = StyleSheet.create({
-  radio: {
-    flexDirection: 'row'
+  radioGroup: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
   },
+  radio: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '50%'
+  },
+  button: { borderWidth: 1 },
   text: {
     alignSelf: 'center'
   }
--- views/NewReport.js
+++ views/NewReport.js
@@ -4,7 +4,20 @@ import { TextInput, Button, Snackbar } from 'react-native-paper';
 import ImagePicker from '../components/reports/ImagePicker';
 import PreviewReport from '../components/reports/PreviewReport';
 import ReportTopics from '../components/reports/ReportTopics';
+import AppLoading from 'expo-app-loading';
 import useCamera from '../hooks/useCamera';
+import {
+  useFonts,
+  Inter_100Thin,
+  Inter_200ExtraLight,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
 
 const NewReport = ({ navigation }) => {
   const { image, video, getImage, launchCamera, setImage } = useCamera({});
@@ -33,67 +46,99 @@ const NewReport = ({ navigation }) => {
     setPreview(false);
     setSnackbar(true);
   };
-  return (
-    <View style={styles.container}>
-      <Button
-        mode="outlined"
-        onPress={openDialog}
-        accessibilityLabel="select a topic to report"
-      >
-        choose report topic
-      </Button>
-      <ReportTopics
-        open={open}
-        closeDialog={closeDialog}
-        checked={checkedTopic}
-        setChecked={handleChecked}
-      />
-      {checkedTopic ? <Text>{checkedTopic} </Text> : null}
-      <View style={styles.description}>
-        <TextInput
-          placeholder="Description of the issue"
-          value={description}
-          onChangeText={(text) => setDescription(text)}
-          multiline={true}
-          mode="outlined"
-          label="description"
-          numberOfLines={3}
-        ></TextInput>
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+    Inter_100Thin,
+    Inter_200ExtraLight,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.mainHeader}>Add a new report</Text>
+          <Text style={styles.subHeader}> Choose report topic</Text>
+        </View>
+        <View>
+          <ReportTopics checked={checkedTopic} setChecked={handleChecked} />
+          <View style={styles.description}>
+            <TextInput
+              placeholder="Description of the issue"
+              value={description}
+              onChangeText={(text) => setDescription(text)}
+              multiline={true}
+              mode="outlined"
+              label="description"
+              numberOfLines={3}
+            ></TextInput>
+          </View>
+          <ImagePicker
+            image={image}
+            video={video}
+            getImage={getImage}
+            launchCamera={launchCamera}
+          />
+          <Button
+            mode="outlined"
+            onPress={openPreview}
+            accessibilityLabel="preview report and send"
+          >
+            submit
+          </Button>
+          <PreviewReport
+            open={isPreviewOpened}
+            closeDialog={handlePreviewClosing}
+            action={handleReportSubmission}
+            topic={checkedTopic}
+            description={description}
+            image={image}
+          />
+          <Snackbar visible={isSnackbarLanuched} onDismiss={closeSnackbar}>
+            Report successfully submitted!
+          </Snackbar>
+        </View>
       </View>
-      <ImagePicker
-        image={image}
-        video={video}
-        getImage={getImage}
-        launchCamera={launchCamera}
-      />
-      <Button
-        mode="outlined"
-        onPress={openPreview}
-        accessibilityLabel="preview report and send"
-      >
-        submit
-      </Button>
-      <PreviewReport
-        open={isPreviewOpened}
-        closeDialog={handlePreviewClosing}
-        action={handleReportSubmission}
-        topic={checkedTopic}
-        description={description}
-        image={image}
-      />
-      <Snackbar visible={isSnackbarLanuched} onDismiss={closeSnackbar}>
-        Report successfully submitted!
-      </Snackbar>
-    </View>
-  );
+    );
+  }
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start'
+  },
+  header: {
+    width: '100%',
+    paddingLeft: '3%',
+    paddingTop: '10%'
+  },
+  mainHeader: {
+    color: '#112454',
+    paddingLeft: '3%',
+    paddingTop: '5%',
+    paddingBottom: '4%',
+    fontSize: 39,
+    width: '100%',
+    textAlign: 'left',
+    display: 'flex',
+    fontFamily: 'Inter_700Bold'
+  },
+  subHeader: {
+    paddingLeft: '4%',
+    paddingBottom: '2%',
+    fontSize: 16,
+    fontFamily: 'Inter_400Regular',
+    color: '#566787'
   },
   description: {
     width: 240,
