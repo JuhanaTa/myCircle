9c11fb86df35c3df726b037f60de19338ba2c626
tamanjic <tamanji.che@metropolia.fi>
2021-10-07 16:13:49 +0300

    profile page

--- views/ProfileScreen.js
+++ views/ProfileScreen.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { Avatar } from 'react-native-paper';
+import UserAvatar from '../components/profile/UserAvatar';
+import useCamera from '../hooks/useCamera';
+import { List } from 'react-native-paper';
 
 const ProfileScreen = ({ navigation }) => {
+  const { image, getImage, launchCamera } = useCamera({ aspect: [4, 3] });
+  const [isExpanded, setAccordion] = useState(false);
+  const openAccordion = () => setAccordion(true);
   return (
     <View style={styles.container}>
-      <Avatar.Icon size={64} icon="account" />
+      <UserAvatar image={image} />
+      <List.Section>
+      <List.Accordion
+        title="Personal Data"
+        left={props => <List.Icon {...props} icon="account" />}
+        expanded={isExpanded}
+        onPress={openAccordion}>
+        <List.Item title="@username" />
+        <List.Item title="first.last@me.fi" />
+      </List.Accordion>
+      <List.Accordion
+        title="Controlled Accordion"
+        left={props => <List.Icon {...props} icon="folder" />}
+        expanded={isExpanded}
+        onPress={openAccordion}>
+        <List.Item title="First item" />
+        <List.Item title="Second item" />
+      </List.Accordion>
+      <List.Accordion
+        title="Controlled Accordion"
+        left={props => <List.Icon {...props} icon="folder" />}
+        expanded={isExpanded}
+        onPress={openAccordion}>
+        <List.Item title="First item" />
+        <List.Item title="Second item" />
+      </List.Accordion>
+    </List.Section>
     </View>
   );
 };
@@ -14,8 +45,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
   }
 });
 
